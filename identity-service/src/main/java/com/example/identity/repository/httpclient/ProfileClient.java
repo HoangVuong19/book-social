f4175441c780abb771d1ff876899ec45e313ae20
HoangVuong <vuonghoang0208@gmail.com>
2025-10-27 23:41:51 +0700

    profile-setting add security among service

--- identity-service/src/main/java/com/example/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/example/identity/repository/httpclient/ProfileClient.java
@@ -5,9 +5,13 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.identity.config.request.AuthenticationRequestInterceptor;
 import com.example.identity.dto.request.ProfileCreationRequest;
 
-@FeignClient(name = "profile-service", url = "${app.services.profile}")
+@FeignClient(
+        name = "profile-service",
+        url = "${app.services.profile}",
+        configuration = {AuthenticationRequestInterceptor.class})
 public interface ProfileClient {
     @PostMapping(value = "/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
     void createProfile(@RequestBody ProfileCreationRequest request);
